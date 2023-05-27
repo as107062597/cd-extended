f253d883672cccab278d1c73ed742fa4be3682b8
Neil Chen <as107062597@outlook.com>
2023-05-27 19:14:29 +0800

    [Update] Add new help documents

--- src/dir/forwardexecutor.cpp
+++ src/dir/forwardexecutor.cpp
@@ -20,15 +20,43 @@ void ForwardExecutor::doExecuteAction() {
 }
 
 std::string ForwardExecutor::document() const {
-    return R"(Usage: nd [NUMBER] [OPTION]
+    return R"(Usage: nd [NUMBER]
+  or:  nd [OPTION]
 Switches the working directory to the next working directory.
 
 It also allows you to navigate to even earlier working directories through
 options and parameters.
 
 Mandatory arguments to long options are mandatory for short options too.
+  -a, --after=DURATION       print the directories visited after a time point
+                               DURATION earlier than the current time
+      --after-timestamp=TIMESTAMP
+                             display the directories visited after TIMESTAMP
+  -b, --before=DURATION      output the directories that were visited prior to
+                               a time point DURATION earlier than the current
+                               time
+      --before-timestamp=TIMESTAMP
+                             display the directories visited before TIMESTAMP
+  -t, --time                 show visited timestamp when list directory history
   -h, --help                 display this help and exit
-  -n, --number=NUMBER        go ahead to the next NUMBER directories)";
+  -l, --list[=SIZE]          exhibit SIZE most recently visited and changeable
+                               directories, defaulting to all directories
+  -n, --number=NUMBER        go ahead to the next NUMBER directories
+  -v, --verbose              explain what is being done
+  -V, --version              output version information and exit
+
+The format of DURATION is <day>d|<hour>h|<minute>m|<second>s. For example, you
+can use the following phrase to express one day and a half: 1d12h
+
+The TIMESTAMP argument is composed of <yyyy>, <MM>, <dd>, <HH>, <mm> and <ss>,
+representing year, month, day, hour, minute and second respectively.
+
+TIMESTAMP format:
+  <HH>:<mm>[:<ss>]
+  [<yyyy>-]<MM>-<dd>
+  [<yyyy>-]<MM>-<dd>T<HH>:<mm>[:<ss>]
+
+More information is available on GitHub: <https://github.com/as107062597/>)";
 }
 
 void ForwardExecutor::assertNumber() const {
